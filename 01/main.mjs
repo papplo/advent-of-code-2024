d1bc23385697f543d6f6c1618fdbb511cfa31f69
Pablo Anttila <pablo.anttila@gmail.com>
2024-12-04 00:03:55 +0000

    aoc.js imports current days scripts. Two stars for first day.

--- 01/main.mjs
+++ 01/main.mjs
@@ -1,3 +1,51 @@
-const main = () => {
-    console.log('gello aoc')
+function primi(criteria) {
+    //01 (a)
+    let sum = 0;
+    const leftlist = [];
+    const rightlist = [];
+
+    for (let index = 0; index < criteria.length; index++) {
+        const [n1, n2] = criteria[index].split('   ');
+        leftlist.push(n1);
+        rightlist.push(n2);
+    }
+
+    leftlist.sort((a, b) => a - b);
+    rightlist.sort((a, b) => a - b);
+
+    for (let index = 0; index < leftlist.length; index++) {
+        const diff = rightlist[index] - leftlist[index];
+
+        sum += Math.abs(diff)
+        console.log(diff, sum)
+    }
+
+    return sum
+
+}
+
+const deuce = (criteria) => {
+    //01 b
+
+    let sum  = 0;
+    let leftlist = [];
+    let rightlist = [];
+
+    for (let index = 0; index < criteria.length; index++) {
+        const [n1, n2] = criteria[index].split('   ');
+        leftlist.push(n1);
+        rightlist.push(n2);
+    }
+
+    leftlist.forEach(n1 => {
+       const simScore = [...rightlist].filter(n2 => n2 === n1).length;
+       sum += n1 * simScore
+       console.log(simScore, sum);
+    })
+
+} 
+
+
+export {
+    primi, deuce
 }
--- aoc.mjs
+++ aoc.mjs
@@ -1,6 +1,9 @@
 import fs from 'fs';
 import path from 'path';
 
+import { primi, deuce } from './01/main.mjs';
+import { Cipher } from 'crypto';
+
 const main = () => {
     const args = process.argv.slice(2);
     if (args.length < 3) {
@@ -10,36 +13,17 @@ const main = () => {
   
     const [day, version, inputFile] = args;
     const criteria = fs.readFileSync(path.join(day, inputFile), 'utf-8').split('\n').filter(Boolean);
-    
-    function test(criteria) {
-      //01 (a)
-      let sum = 0;
-      const leftlist = [];
-      const rightlist = [];
-
-      for (let index = 0; index < criteria.length; index++) {
-        const [n1, n2] = criteria[index].split('   ');
-        leftlist.push(n1);
-        rightlist.push(n2);
-      }
-
-      leftlist.sort((a,b) => a-b);
-      rightlist.sort((a,b) => a-b);
-
-      for (let index = 0; index < leftlist.length; index++) {
-        const diff = rightlist[index] - leftlist[index];
-
-        sum += Math.abs(diff)
-        console.log(diff, sum)
-      }
-
-      return sum
-
+   
+    let output;
+    if (version == 1) {
+    output = primi(criteria)
+      console.log('asdsdasdasd')
+    } else {
+      output = deuce(criteria)
     }
+   
 
-    const output = test(criteria);
-
-    console.log('output: ', day, output)
+    console.log('day:', day, 'part:', version, output)
   };
   
 main()
